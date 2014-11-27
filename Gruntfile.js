5b17360dae8a65f7cda6ffee89f3db974008ed84
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-11-28 00:06:02 +0300

    Add a locally working protractor test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var spawn = require('child_process').spawn;
+
 module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
@@ -5,21 +7,92 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     connect: {
-      server: {
+      // server: {
+      //   options: {
+      //     port: 9000,
+      //     hostname: '0.0.0.0',
+      //     middleware: function (connect) {
+      //       return [
+      //         connect().use(
+      //           '/bower_components',
+      //           connect.static('./bower_components')
+      //         ),
+      //         connect.static('src')
+      //       ];
+      //     }
+      //   }
+      // },
+      testserver: {
         options: {
-          port: 9000,
+          port: 8000,
           hostname: '0.0.0.0',
-          middleware: function (connect) {
+          middleware: function (connect, options) {
+            console.log('options.base', options.base);
+            var base = Array.isArray(options.base) ? options.base[options.base.length - 1] : options.base;
             return [
-              connect().use(
-                '/bower_components',
-                connect.static('./bower_components')
-              ),
-              connect.static('src')
+              connect.static(base)
             ];
           }
         }
       }
+    },
+    protractor: {
+      normal: 'test/protractor.conf.js'
     }
   });
+
+  function updateWebdriver (done) {
+    var p = spawn('node', ['node_modules/protractor/bin/webdriver-manager', 'update']);
+    p.stdout.pipe(process.stdout);
+    p.stderr.pipe(process.stderr);
+    p.on('exit', function(code){
+      if(code !== 0) grunt.fail.warn('Webdriver failed to update');
+      done();
+    });
+  }
+
+  function startProtractor (config, done) {
+    // var sauceUser = grunt.option('sauceUser');
+    // var sauceKey = grunt.option('sauceKey');
+    // var tunnelIdentifier = grunt.option('capabilities.tunnel-identifier');
+    // var sauceBuild = grunt.option('capabilities.build');
+    // var browser = grunt.option('browser');
+    // var specs = grunt.option('specs');
+    var args = ['node_modules/protractor/bin/protractor', config];
+    // if (sauceUser) args.push('--sauceUser=' + sauceUser);
+    // if (sauceKey) args.push('--sauceKey=' + sauceKey);
+    // if (tunnelIdentifier) args.push('--capabilities.tunnel-identifier=' + tunnelIdentifier);
+    // if (sauceBuild) args.push('--capabilities.build=' + sauceBuild);
+    // if (specs) args.push('--specs=' + specs);
+    // if (browser) {
+    //   args.push('--browser=' + browser);
+    // }
+
+
+    var p = spawn('node', args);
+    p.stdout.pipe(process.stdout);
+    p.stderr.pipe(process.stderr);
+    p.on('exit', function(code){
+      if(code !== 0) grunt.fail.warn('Protractor test(s) failed. Exit code: ' + code);
+      done();
+    });
+  }
+
+  grunt.registerTask('webdriver', 'Update webdriver', function() {
+    updateWebdriver(this.async());
+  });
+
+  grunt.registerMultiTask('protractor', 'Run Protractor integration tests', function() {
+    startProtractor(this.data, this.async());
+  });
+
+  grunt.registerTask(
+    'test:protractor',
+    'Run the end to end tests with Protractor and keep a test server running in the background',
+    [
+      'webdriver',
+      'connect:testserver',
+      'protractor:normal'
+    ]
+  );
 };
--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,4 +1,8 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['*.spec.js']
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
+  // seleniumServerJar: '/usr/local/lib/node_modules/protractor/selenium/selenium-server-standalone-2.44.0.jar',
+  // sauceUser: 'pomerantsevp',
+  // sauceKey: '497ab04e-f31b-4a7b-9b18-ae3fbe023222',
+  specs: ['**/*.spec.js'],
+  baseUrl: 'http://localhost:8000/'
 };
