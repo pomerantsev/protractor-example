7031c371ad6d004a71f311a5088a6c72eaf2acad
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-11-29 18:51:11 +0300

    Add a couple more tests

--- test/spec/ng-infinite-scroll.spec.js
+++ test/spec/ng-infinite-scroll.spec.js
@@ -1,13 +1,29 @@
 describe('ng-infinite-scroll', function () {
   it('should be triggered when page is scrolled to the bottom', function () {
     browser.get('test/examples/basic.html');
-
     var items = element.all(by.repeater('item in items'));
-
-    expect(items.last().getText()).toBe('100');
-
+    expect(items.count()).toBe(100);
     browser.driver.executeScript('window.scrollTo(0, document.body.scrollHeight)');
+    expect(items.count()).toBe(200);
+  });
 
-    expect(items.last().getText()).toBe('200');
+  it('triggers immediately by default', function () {
+    browser.get('test/examples/immediately.html');
+    var items = element.all(by.repeater('item in items'));
+    expect(items.count()).toBe(100);
+  });
+
+  it('does not trigger immediately when infinite-scroll-immediate-check is false', function () {
+    browser.get('test/examples/immediate-check-off.html');
+    var items = element.all(by.repeater('item in items'));
+    expect(items.count()).toBe(0);
+  });
+
+  it('respects the disabled attribute', function () {
+    browser.get('test/examples/disabled.html');
+    var items = element.all(by.repeater('item in items'));
+    expect(items.count()).toBe(0);
+    element(by.id('action')).click();
+    expect(items.count()).toBe(100);
   });
 });
