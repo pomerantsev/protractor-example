d725735b13d5d767dac8e62cd64947a22b740295
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-11-29 21:47:21 +0300

    More refactoring

--- test/spec/ng-infinite-scroll.spec.js
+++ test/spec/ng-infinite-scroll.spec.js
@@ -23,37 +23,33 @@ describe('ng-infinite-scroll', function () {
   it('should be triggered immediately and when page is scrolled to the bottom', function () {
     replace('content', '<div infinite-scroll="loadMore()">' + itemsMarkup + '</div>');
     browser.get(pathToDocument);
-    var items = getItems();
-    expect(items.count()).toBe(100);
+    expect(getItems().count()).toBe(100);
     browser.driver.executeScript('window.scrollTo(0, document.body.scrollHeight)');
-    expect(items.count()).toBe(200);
+    expect(getItems().count()).toBe(200);
   });
 
   it('does not trigger immediately when infinite-scroll-immediate-check is false', function () {
     replace('content', '<div infinite-scroll="loadMore()" infinite-scroll-immediate-check="false">' + itemsMarkup + '</div>');
     browser.get(pathToDocument);
-    var items = getItems();
-    expect(items.count()).toBe(0);
+    expect(getItems().count()).toBe(0);
   });
 
   it('respects the disabled attribute', function () {
     browser.get('test/examples/disabled.html');
-    var items = getItems();
-    expect(items.count()).toBe(0);
+    expect(getItems().count()).toBe(0);
     element(by.id('action')).click();
-    expect(items.count()).toBe(100);
+    expect(getItems().count()).toBe(100);
   });
 
   it('respects the infinite-scroll-distance attribute', function () {
     browser.get('test/examples/distance.html');
-    var items = getItems();
-    expect(items.count()).toBe(100);
+    expect(getItems().count()).toBe(100);
     // 2 * window.innerHeight means that the bottom of the screen should be somewhere close to
     // body height - window height. That means that the top of the window is body height - 2 * window height.
     // Why can't we even set -10 here? Looks like it also takes the last element's height into account
     browser.driver.executeScript('window.scrollTo(0, document.body.scrollHeight - 2 * window.innerHeight - 20)');
-    expect(items.count()).toBe(100);
+    expect(getItems().count()).toBe(100);
     browser.driver.executeScript('window.scrollTo(0, document.body.scrollHeight - 2 * window.innerHeight)');
-    expect(items.count()).toBe(200);
+    expect(getItems().count()).toBe(200);
   });
 });
