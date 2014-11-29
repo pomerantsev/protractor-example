aa540becdf3fad94e3dfd4383c01fd46f85be766
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-11-29 19:40:29 +0300

    Do some refactoring

--- test/spec/ng-infinite-scroll.spec.js
+++ test/spec/ng-infinite-scroll.spec.js
@@ -1,7 +1,12 @@
 describe('ng-infinite-scroll', function () {
+
+  function getItems () {
+    return element.all(by.repeater('item in items'));
+  }
+
   it('should be triggered when page is scrolled to the bottom', function () {
     browser.get('test/examples/basic.html');
-    var items = element.all(by.repeater('item in items'));
+    var items = getItems();
     expect(items.count()).toBe(100);
     browser.driver.executeScript('window.scrollTo(0, document.body.scrollHeight)');
     expect(items.count()).toBe(200);
@@ -9,19 +14,19 @@ describe('ng-infinite-scroll', function () {
 
   it('triggers immediately by default', function () {
     browser.get('test/examples/immediately.html');
-    var items = element.all(by.repeater('item in items'));
+    var items = getItems();
     expect(items.count()).toBe(100);
   });
 
   it('does not trigger immediately when infinite-scroll-immediate-check is false', function () {
     browser.get('test/examples/immediate-check-off.html');
-    var items = element.all(by.repeater('item in items'));
+    var items = getItems();
     expect(items.count()).toBe(0);
   });
 
   it('respects the disabled attribute', function () {
     browser.get('test/examples/disabled.html');
-    var items = element.all(by.repeater('item in items'));
+    var items = getItems();
     expect(items.count()).toBe(0);
     element(by.id('action')).click();
     expect(items.count()).toBe(100);
@@ -29,7 +34,7 @@ describe('ng-infinite-scroll', function () {
 
   it('respects the infinite-scroll-distance attribute', function () {
     browser.get('test/examples/distance.html');
-    var items = element.all(by.repeater('item in items'));
+    var items = getItems();
     expect(items.count()).toBe(100);
     // 2 * window.innerHeight means that the bottom of the screen should be somewhere close to
     // body height - window height. That means that the top of the window is body height - 2 * window height.
